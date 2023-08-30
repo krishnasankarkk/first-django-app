8c06d2feb98cd8c699221de8a8ca0685f7421822
krishnasankar <sankarkrishna689@gmail.com>
2023-08-30 18:18:55 +0530

    Initial commit

---
+++ polls/models.py
@@ -0,0 +1,28 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    """Function to store Question"""
+
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    """Function to store Choices."""
+
+    question_reference = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice
