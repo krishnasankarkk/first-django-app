296320481b098d10b3507eaaa64757e135768532
krishnasankar <sankarkrishna689@gmail.com>
2023-09-04 12:04:05 +0530

    changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,11 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
-# # Initializes environment variables.
-# env = os.path.join(BASE_DIR, ".env")
-# if os.path.isfile(env):
-#     dotenv.load_dotenv(env)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
